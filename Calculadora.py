52b9d08e1b0822bb29077c2f372fbc15bde3dbf8
JuanCarlosOntiveros <jcarloson79@gmail.com>
2023-06-15 18:18:21 -0600

    modified:   Calculadora.py

--- Calculadora.py
+++ Calculadora.py
@@ -47,4 +47,24 @@ button_equal = tk.Button(window, text="=", padx=49, pady=10, command=button_equa
 button_clear = tk.Button(window, text="Clear", padx=40, pady=10, command=button_clear)
 
 
-root.mainloop()
+# Posicionar los botones en la ventana
+button_1.grid(row=1, column=0)
+button_2.grid(row=1, column=1)
+button_3.grid(row=1, column=2)
+button_4.grid(row=2, column=0)
+button_5.grid(row=2, column=1)
+button_6.grid(row=2, column=2)
+button_7.grid(row=3, column=0)
+button_8.grid(row=3, column=1)
+button_9.grid(row=3, column=2)
+button_0.grid(row=4, column=0)
+
+button_add.grid(row=1, column=3)
+button_subtract.grid(row=2, column=3)
+button_multiply.grid(row=3, column=3)
+button_divide.grid(row=4, column=3)
+button_equal.grid(row=4, column=1, columnspan=2)
+button_clear.grid(row=5, column=0, columnspan=4)
+
+# Ejecutar el bucle principal
+window.mainloop()
