4e99a46e223f14f9548c5b780e14e32445cd0941
ricardopuente203 <ricardopuente203@gmail.com>
2023-06-15 18:09:59 -0600

    Ricardo

--- Calculadora.py
+++ Calculadora.py
@@ -4,6 +4,13 @@ import tkinter as tk
 root = tk.Tk()
 root.title("Calculadora")
 
+def button_click(number):
+    current = entry.get()
+    entry.delete(0, tk.END)
+    entry.insert(tk.END, current + str(number))
+
+def button_clear():
+    entry.delete(0, tk.END)
 
 
 
